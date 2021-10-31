a57e9e9900b8d47df37dee2b10b065236ea5439d
WolverinDEV <git@did.science>
2021-10-31 10:42:30 +0100

    Directly providing a .npmrc file [ci release]

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,2 @@
-cat > ~/.npmrc << EOF
-msvs_version=2017
-//registry.npmjs.org/:_authToken=${npm_auth_token}
-registry=http://registry.npmjs.org/
-maxsockets=5
-EOF
-
 cd dist || exit 1
 npm publish
